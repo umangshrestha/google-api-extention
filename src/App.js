56d77770e108b0c538ffc435fe2082dfc30760c4
uamng <umang@lifesignals.com>
2022-05-10 18:13:24 -0400

    add:label

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 import Button from './components/button';
 
 function App() {
+  useEffect(()=> {
+    document.title ="Google Console Extention"
+  })
   return (
     <div className="App">
       <header className="App-header">
-       <Button name="start" />
+        <label htmlFor="num"> number of API connections </label>
+        <input type="text" id="num" name="name" required minLength={0} maxLength={4} size={10} defaultValue={10} />
+        <Button name="Save Settings" />
+
+        <h2>Actions and Details</h2>
+        <Button name="Download CSV" label="Download/Export Saved Data"/>
+        <Button name="Delete saved items" id="delete" label="Clean saved keywords data" />
+                      
       </header>
     </div>
   );
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,15 @@
 import './button.css'
+import './label.css'
 
-const Button = (props) => <button onClick={props.onClick}> {props.name}</button> 
 
-export default Button;
+const Button = (props) => {
+    let label;
+    if (props.label !== undefined)
+        label = <label htmlFor={props.name}> {props.label} </label>
+    
+    return (<>
+        {label}
+        <button onClick={props.onClick}> {props.name}</button>
+    </>)
+}
+export default Button ;
