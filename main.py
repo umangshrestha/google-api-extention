b317ca9b8e34905457cddc7ee6a02cfcfae13823
nipur <Nimisha.Purushan@octasic.com>
2022-05-11 17:25:27 -0400

    response format

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from urllib import response
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 from flask import Flask, current_app, g, request, Response
+from datetime import datetime
 import requests
 import json
 import time
 import pandas as pd
 from csv import writer
-import os
-import threading
 from flask_cors import CORS, cross_origin
 from flask import Flask, request, jsonify, make_response
 
@@ -41,12 +40,9 @@ def index():
     json_key = request.json["json_data"]
     write_json(json_key)
     ob.process_url("temp.json",url)
-    response = jsonify({"status": 1})
+    response = jsonify(ob.process_url("temp.json",url))
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
-   
-
 
 
 def write_json(json_data):
@@ -56,11 +52,13 @@ def write_json(json_data):
 class Credential:
     def _init_(self):
         self._request_responses = []
-        self.final_response_list = []
         self.isHeader = True
 
     def process_url(self, json_path, url_list):
+        final_response_list = []
         for url in url_list:
+            if not url:
+                continue
             self._json_key_file = json_path
             body_content = "{url: \"%s\", type: \"URL_UPDATED\"}" % url
             cred = ServiceAccountCredentials.from_json_keyfile_name(
@@ -68,14 +66,15 @@ class Credential:
             http = cred.authorize(httplib2.Http())
             try:
                 response, content = http.request(
-                    ENDPOINT, method="POST", header={        'Access-Control-Allow-Origin' : '*',
-},body=body_content)
-            except:
-                response = httplib2.Response({'status': -1})
-        return response
+                    ENDPOINT, method="POST",body=body_content)
+                final_response_list.append({"url": url, "time": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), "status":response.status})
+            except Exception as e:
+                print(e)
+                final_response_list.append({"url": url, "time": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'status': -1})
+        print(final_response_list)
+        return final_response_list
 
 
 if __name__ == '__main__':
     app.debug=True
     app.run(port=5000)
-
